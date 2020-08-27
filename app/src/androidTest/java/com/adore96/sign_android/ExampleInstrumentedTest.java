f033a4bc76cb19251279d329405e5d2f7132ad0d
Kalhara Perera <adorekasun@gmail.com>
2020-08-27 17:05:18 +0530

    Project Updated without Datafile

--- app/src/androidTest/java/com/adore96/sign_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/adore96/sign_android/ExampleInstrumentedTest.java
@@ -2,13 +2,15 @@ package com.adore96.sign_android;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +19,13 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
     @Test
-    public void useAppContext() {
+    public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.adore96.sign_android", appContext.getPackageName());
+
+        assertEquals("com.adore96.tflite", appContext.getPackageName());
     }
 }
+
