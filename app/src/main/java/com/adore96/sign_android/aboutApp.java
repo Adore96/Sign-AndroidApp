cd647436d7dd4899d8036c66f7624c1668d49a3e
Kalhara Perera <adorekasun@gmail.com>
2020-10-22 23:49:31 +0530

    Update

---
+++ app/src/main/java/com/adore96/sign_android/aboutApp.java
@@ -0,0 +1,14 @@
+package com.adore96.sign_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aboutApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
