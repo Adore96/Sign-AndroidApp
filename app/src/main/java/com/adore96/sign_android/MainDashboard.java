af2101f4c84f569a6588e9f1119bba8993c146b3
Kalhara Perera <adorekasun@gmail.com>
2020-10-24 16:27:51 +0530

    MainDashboard Updated

--- app/src/main/java/com/adore96/sign_android/MainDashboard.java
+++ app/src/main/java/com/adore96/sign_android/MainDashboard.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class mainDashboard extends AppCompatActivity {
+public class MainDashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
