a491821b50977b2d2dab479340cfdb8ff04b1f6d
Kalhara Perera <adorekasun@gmail.com>
2020-11-01 15:19:59 +0530

    updated to CardView

--- app/src/main/java/com/adore96/sign_android/MainDashboard.java
+++ app/src/main/java/com/adore96/sign_android/MainDashboard.java
@@ -1,14 +1,65 @@
 package com.adore96.sign_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainDashboard extends AppCompatActivity {
+public class MainDashboard extends AppCompatActivity implements View.OnClickListener{
+
+    private CardView liveFeed,imageTrans,videoTrans,generalSigns,openGallery,contactAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dashboard);
+
+        liveFeed = (CardView) findViewById(R.id.livefeed);
+        imageTrans = (CardView) findViewById(R.id.imageTrans);
+        videoTrans = (CardView) findViewById(R.id.videoTrans);
+        generalSigns = (CardView) findViewById(R.id.generalSigns);
+        openGallery = (CardView) findViewById(R.id.openGallery);
+        contactAuth = (CardView) findViewById(R.id.contactAuth);
+
+        liveFeed.setOnClickListener(this);
+        imageTrans.setOnClickListener(this);
+        videoTrans.setOnClickListener(this);
+        generalSigns.setOnClickListener(this);
+        openGallery.setOnClickListener(this);
+        contactAuth.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i ;
+
+        switch (view.getId()){
+            case R.id.livefeed :
+                i = new Intent(this,liveFeed.class);
+                startActivity(i);
+                break;
+
+            case R.id.image :
+                i = new Intent(this,ImagePred.class);
+                startActivity(i);
+                break;
+
+            case R.id.video :
+                i = new Intent(this,VideoPred.class);
+                startActivity(i);
+                break;
+
+            case R.id.general :
+                i = new Intent(this,GeneralInfo.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+
+        }
     }
 }
--- app/src/main/java/com/adore96/sign_android/liveFeed.java
+++ app/src/main/java/com/adore96/sign_android/liveFeed.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class basicSigns extends AppCompatActivity {
+public class liveFeed extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic_signs);
+        setContentView(R.layout.activity_live_feed);
     }
 }
