7fd0add495def54c4f2d2475181e8fcb0d7ba5cd
Kalhara Perera <adorekasun@gmail.com>
2020-11-05 01:32:00 +0530

    Working :100: Added Contributions Page

--- app/src/main/java/com/adore96/sign_android/aboutApp.java
+++ app/src/main/java/com/adore96/sign_android/aboutApp.java
@@ -1,8 +1,10 @@
 package com.adore96.sign_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class aboutApp extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class aboutApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+
+    }
+
+    public void goToAnActivity(View view) {
+        Intent intent = new Intent(this, contributions.class);
+        startActivity(intent);
     }
 }
